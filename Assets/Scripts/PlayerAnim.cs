7ba14d4be3df94859f5e648b7ae10410c9911fbf
ooshka <wadeyalex@gmail.com>
2023-01-06 18:18:20 -0800

    Add basic animations

--- Assets/Scripts/PlayerAnim.cs
+++ Assets/Scripts/PlayerAnim.cs
@@ -6,6 +6,7 @@ public class PlayerAnim : MonoBehaviour
 {
 
     private PlayerMotor motor;
+    private Animator anim;
 
     [HideInInspector]
     public enum State
@@ -13,21 +14,38 @@ public class PlayerAnim : MonoBehaviour
         WALKING, SPRINTING, IDLE, CROUCH_IDLE, CROUCH_WALKING, SLIDING, AIRBORNE, MELEE, JUMPING
     }
 
+    private readonly string MELEE_TRIGGER = "Melee_Trigger";
+    private readonly string JUMPING_TRIGGER = "Jump_Trigger";
+    private readonly string MOVEMENT_BLEND = "Movement_Blend";
+
+    private readonly float blendTime = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
         motor = GetComponent<PlayerMotor>();
+        anim = GameObject.Find("Player_Arms").GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         List<State> states = motor.GetState();
-        string stateString = "";
-        foreach (State state in states)
+        
+        if (states.Contains(State.MELEE))
         {
-            stateString += " " + state.ToString();
+            anim.SetTrigger(MELEE_TRIGGER);
+        } else if (states.Contains(State.JUMPING))
+        {
+            anim.SetTrigger(JUMPING_TRIGGER);
+        } else if (states.Contains(State.SPRINTING))
+        {
+            anim.SetFloat(MOVEMENT_BLEND, 1f, blendTime, Time.deltaTime);
+        } else if (states.Contains(State.IDLE))
+        {
+            anim.SetFloat(MOVEMENT_BLEND, 0f, blendTime, Time.deltaTime);
+        } else if (states.Contains(State.WALKING)) {
+            anim.SetFloat(MOVEMENT_BLEND, 0.5f, blendTime, Time.deltaTime);
         }
-        Debug.Log(stateString);
     }
 }
