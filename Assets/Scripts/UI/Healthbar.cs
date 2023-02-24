fb97a2a0d1eab0e055f2dc6bf070f1f654036f80
ooshka <wadeyalex@gmail.com>
2023-02-23 17:52:13 -0800

    add basic healthbar

--- Assets/Scripts/UI/Healthbar.cs
+++ Assets/Scripts/UI/Healthbar.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Healthbar : MonoBehaviour
 {
+
+    private Slider primaryHealth, secondaryHealth;
+
+    public float _damageAmount = 20f;
+    public float _maxHealth = 100f;
+    public float _secondaryHealthSpeed = 0.3f;
+    private float _currentHealth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        primaryHealth = transform.Find("PrimaryHealth").GetComponent<Slider>();
+        secondaryHealth = transform.Find("SecondaryHealth").GetComponent<Slider>();
+
+        primaryHealth.value = _maxHealth;
+        secondaryHealth.value = _maxHealth;
+        _currentHealth = _maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (primaryHealth.value < secondaryHealth.value)
+        {
+            UpdateSecondaryHealth();
+        }
     }
+
+    public void ApplyDamage(float damage)
+    {
+        _currentHealth -= damage;
+        float newSliderValue = Mathf.Max(0, _currentHealth / _maxHealth);
+        primaryHealth.value = newSliderValue;
+    }
+
+    void UpdateSecondaryHealth()
+    {
+        secondaryHealth.value -= _secondaryHealthSpeed * Time.deltaTime;
+    }
+
 }
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,16 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
 
     private Crosshair crosshair;
-
+    private Healthbar playerHealthBar;
+    private Timer damageTimer;
     // Start is called before the first frame update
     void Start()
     {
         crosshair = GetComponent<Crosshair>();
+        playerHealthBar = GetComponentInChildren<Healthbar>();
         crosshair.generateCrosshair(gameObject);
+
+        damageTimer = new Timer(2f, false);
+    }
+
+    private void Update()
+    {
+        if (damageTimer.CanTriggerEventAndReset())
+        {
+            playerHealthBar.ApplyDamage(20f);
+        }
+        damageTimer.Iterate(Time.deltaTime);
+    }
+
+    private void DamageHealthBar()
+    {
+        playerHealthBar.ApplyDamage(20f);
     }
 }
