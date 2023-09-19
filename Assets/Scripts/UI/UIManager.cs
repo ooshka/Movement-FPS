6b0b489b70413afa791e73581857fafc7928da01
ooshka <wadeyalex@gmail.com>
2023-09-19 09:08:18 -0700

    Remove auto damaging healthbar

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -8,28 +8,17 @@ public class UIManager : MonoBehaviour
 
     private Crosshair crosshair;
     private Healthbar playerHealthBar;
-    private Timer damageTimer;
     // Start is called before the first frame update
     void Start()
     {
         crosshair = GetComponent<Crosshair>();
         playerHealthBar = GetComponentInChildren<Healthbar>();
         crosshair.generateCrosshair(gameObject);
-
-        damageTimer = new Timer(2f, false);
     }
 
     private void Update()
     {
-        if (damageTimer.CanTriggerEventAndReset())
-        {
-            playerHealthBar.ApplyDamage(20f);
-        }
-        damageTimer.Iterate(Time.deltaTime);
+
     }
 
-    private void DamageHealthBar()
-    {
-        playerHealthBar.ApplyDamage(20f);
-    }
 }
